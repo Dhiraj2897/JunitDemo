0faa2eb7c06c06bba46cbb23cc93cecd4af739d1
Dhiraj2897 <dhirajbhosale2897@gmail.com>
2025-02-06 00:01:24 +0530

    created Junit 5 testcases

--- src/test/java/com/dhiraj/services/CalculatorServiceTest.java
+++ src/test/java/com/dhiraj/services/CalculatorServiceTest.java
@@ -1,50 +1,50 @@
-package com.dhiraj.services;
-
-import org.junit.*;
-
-import java.util.Date;
-
-public class CalculatorServiceTest {
-
-
-    @BeforeClass
-    public static void init(){
-        System.out.println("before all the testcase:");
-        System.out.println("Started test :"+ new Date());
-    }
-    @Before
-    public  void beforeEach(){
-        System.out.println("Before each testcase");
-    }
-    //test method of addtwo methods
-    @Test
-    public  void addTwoNumbersTest(){
-        int result = CalculatorService.sumAnyNumbers(12, 45);
-        int exprected=57;
-        Assert.assertEquals(exprected, result);
-        System.out.println("TC001 is passed ");
-
-
-    }
-@Test
-   public void sumAnyNumbersTest(){
-       int result =   CalculatorService.sumAnyNumbers(3,5,3,3,5);
-        int expectedResult=19;
-        Assert.assertEquals(expectedResult, result);
-
-            System.out.println("TC002 is passed");
-
-   }
-   @AfterClass
-   public static void cleanUp(){
-       System.out.println("after all testcases");
-       System.out.println("End test cases :"+new Date());
-   }
-
-
-
-
-
-
-
-}
+//package com.dhiraj.services;
+//
+//import org.junit.*;
+//
+//import java.util.Date;
+//
+//public class CalculatorServiceTest {
+//
+//
+//    @BeforeClass
+//    public static void init(){
+//        System.out.println("before all the testcase:");
+//        System.out.println("Started test :"+ new Date());
+//    }
+//    @Before
+//    public  void beforeEach(){
+//        System.out.println("Before each testcase");
+//    }
+//    //test method of addtwo methods
+//    @Test
+//    public  void addTwoNumbersTest(){
+//        int result = CalculatorService.sumAnyNumbers(12, 45);
+//        int exprected=57;
+//        Assert.assertEquals(exprected, result);
+//        System.out.println("TC001 is passed ");
+//
+//
+//    }
+//@Test
+//   public void sumAnyNumbersTest(){
+//       int result =   CalculatorService.sumAnyNumbers(3,5,3,3,5);
+//        int expectedResult=19;
+//        Assert.assertEquals(expectedResult, result);
+//
+//            System.out.println("TC002 is passed");
+//
+//   }
+//   @AfterClass
+//   public static void cleanUp(){
+//       System.out.println("after all testcases");
+//       System.out.println("End test cases :"+new Date());
+//   }
+//
+//
+//
+//
+//
+//
+//
+//}
---
+++ src/test/java/com/dhiraj/services/CalculatorServiceTestJunit5.java
@@ -0,0 +1,20 @@
+package com.dhiraj.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorServiceTestJunit5 {
+
+   @Test
+    public  void addTwoNumbersTest(){
+      int actualResult= CalculatorService.addTwoNumber(3,4);
+      int expectedResult= 7;
+       Assertions.assertEquals(expectedResult,actualResult, "TC001");
+    }
+    @Test
+    public  void addAnyNumbersTest() {
+int actualResult= CalculatorService.sumAnyNumbers(3,4,5,6,1);
+int expectedResult=19;
+Assertions.assertEquals(expectedResult,actualResult,"TC002");
+    }
+}
