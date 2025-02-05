7ec087d78dccfdf42e65783a106d04d24227fdda
Dhiraj2897 <dhirajbhosale2897@gmail.com>
2025-02-05 23:23:13 +0530

    created Junit 4 testcases

--- src/test/java/com/dhiraj/services/CalculatorServiceTest.java
+++ src/test/java/com/dhiraj/services/CalculatorServiceTest.java
@@ -1,24 +1,44 @@
 package com.dhiraj.services;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.Date;
 
 public class CalculatorServiceTest {
 
+
+    @BeforeClass
+    public static void init(){
+        System.out.println("before all the testcase:");
+        System.out.println("Started test :"+ new Date());
+    }
     //test method of addtwo methods
     @Test
     public  void addTwoNumbersTest(){
         int result = CalculatorService.sumAnyNumbers(12, 45);
-        int exprected=56;
+        int exprected=57;
         Assert.assertEquals(exprected, result);
+        System.out.println("TC001 is passed ");
 
-        //actual result
 
-
-        //expected result
     }
+@Test
+   public void sumAnyNumbersTest(){
+       int result =   CalculatorService.sumAnyNumbers(3,5,3,3,5);
+        int expectedResult=19;
+        Assert.assertEquals(expectedResult, result);
 
+            System.out.println("TC002 is passed");
 
+   }
+   @AfterClass
+   public static void cleanUp(){
+       System.out.println("after all testcases");
+       System.out.println("End test cases :"+new Date());
+   }
 
 
 
