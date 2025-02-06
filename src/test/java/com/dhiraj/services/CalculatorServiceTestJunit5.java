95f55f41fbd4cdd11f638258a8ca56e619f10df2
Dhiraj2897 <dhirajbhosale2897@gmail.com>
2025-02-06 08:51:08 +0530

    created Junit 5 testcases

--- src/test/java/com/dhiraj/services/CalculatorServiceTestJunit5.java
+++ src/test/java/com/dhiraj/services/CalculatorServiceTestJunit5.java
@@ -1,29 +1,48 @@
 package com.dhiraj.services;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class CalculatorServiceTestJunit5 {
-
+// execute logic before all the testcases
+    @BeforeAll
+    public static  void init(){
+        System.out.println("this is logic execute before all");
+    }
    @Test
     public  void addTwoNumbersTest(){
       int actualResult= CalculatorService.addTwoNumber(3,4);
       int expectedResult= 7;
        Assertions.assertEquals(expectedResult,actualResult, "TC001");
+       System.out.println("TC001 is passed");
     }
     @Test
     public  void addAnyNumbersTest() {
 int actualResult= CalculatorService.sumAnyNumbers(3,4,5,6,1);
 int expectedResult=19;
 Assertions.assertEquals(expectedResult,actualResult,"TC002");
+        System.out.println("TC002 is passed");
     }
 
 
     @Test
     public void divideTwoNumbers(){
         double actualResult =CalculatorService.divideTwoNumbers(5,2);
-        double expectedResult=3;
+        double expectedResult=2;
         Assertions.assertEquals(expectedResult,actualResult,"TC003");
+        System.out.println("TC003 is passed");
 
+    }
+    @AfterAll
+    public static void cleanUp(){
+        System.out.println(" it is executed after all the testcases");
+    }
+    @BeforeEach
+    public  void BeforeachTestCase(){
+        System.out.println(" it is executed before each  the testcases");
+    }
+    @AfterEach
+    @Disabled
+    public  void AfterEachTestCase(){
+        System.out.println(" it is executed after each the testcases");
     }
 }
